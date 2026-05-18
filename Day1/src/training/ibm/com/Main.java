1212008c5850057aa257ecbe07aab552a1dd00d6
Aaron Josh Baon <aaron.josh.baon@ibm.com>
2026-05-18 11:09:52 +0800

    first commit

---
+++ Day1/src/training/ibm/com/Main.java
@@ -0,0 +1,15 @@
+package training.ibm.com;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+
+	public int blackjack(int a, int b) {
+		return a;
+	}
+
+}
