a7c9c380856776600e04e78004ec13b7fb2823c1
shironzi <aaronbaon1@gmail.com>
2026-05-18 15:43:37 +0800

    finished activities

--- Day1/src/training/ibm/com/Main.java
+++ Day1/src/training/ibm/com/Main.java
@@ -7,12 +7,80 @@ public class Main {
 		System.out.println(blackjack(21, 22));
 		System.out.println(blackjack(2, 22));
 		System.out.println(blackjack(2, 10));
+		
+		System.out.println(dayToStrSwitch());
+		System.out.println(dayToStrPattern());
+		
+		pyramid();
 	}
 	
 	public static int blackjack(int a, int b) {
-		// TODO Auto-generated method stub
 		a = a > 21 ? 0 : a;
 		b = b > 21 ? 0 : b;
 		return a > b ? a : b;
+	}
+	
+	public static String dayToStrSwitch() {
+		Scanner scanner = new Scanner(System.in);
+				
+		System.out.print("Enter Size of Zigzag: ");
+		int day = scanner.nextInt();
+				
+		switch(day) {
+			case 1:
+				return "Monday";
+			case 2:
+				return "Tuesday";
+			case 3:
+				return "Wednesday";
+			case 4:
+				return "Thursday";
+			case 5:
+				return "Friday";
+			case 6:
+				return"Saturday";
+			case 7:
+				return "Sunday";
+			default:
+				return "Invalid day";
+		}
+	}
+	
+	public static String dayToStrPattern() {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter Size of Zigzag: ");
+		int day = scanner.nextInt();
+				
+		return switch(day) {
+			case 1 -> "Monday";
+			case 2 -> "Tuesday";
+			case 3 -> "Wednesday";
+			case 4 -> "Thursday";
+			case 5 -> "Friday";
+			case 6 -> "Saturday";
+			case 7 -> "Sunday";
+			default -> "Invalid day";
+		};
+	}
+	
+	public static void pyramid() {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter Size of Pyramid: ");
+		int size = scanner.nextInt();
+		
+		while(size < 1 || size > 20){
+			System.out.print("Enter Size of Pyramid: ");
+			size = scanner.nextInt();
+		};
+		
+		for(int col=1; col<=size; col++) {
+			for(int row=1; row<=col; row++) {
+				System.out.print(row + " ");
+			}
+			
+			System.out.println();
+		}
 	}
 }
